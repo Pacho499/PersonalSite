0e34b25427d48fdeb547eac881bb0aff6da5978d
Lorenzo <lorenzopalumbo.dev@gmail.com>
2023-03-01 00:36:07 +0100

    set footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,38 @@
-import "../style/Footer.scss";
-
+import '../style/Footer.scss'
 const Footer = () => {
   const url = process.env.PUBLIC_URL
   return (
-    <div className="footerContainer">
+    <div className="border-orange border-t-4 bg-main flex justify-around items-center py-8">
       <div className="contacts">
-        <h5>contatti</h5>
-        <p>lorenzopalumbo.dev@gmail.com</p>
-        <div className="social">
+        <h5 className='text-2xl mb-2'>Contatti</h5>
+        <p className='text-xl mb-2'>lorenzopalumbo.dev@gmail.com</p>
+        <div className="flex justify-around">
           <a
             href="https://www.linkedin.com/in/lorenzo-palumbo-938b0724a/"
             target="_blank"
             rel="noreferrer"
           >
-            <img height="40px" src={url + ('/images/linkedin.webp')} alt="social" />
+            <img style={{height:'40px'}} src={url + ('/images/linkedin.webp')} alt="social" />
           </a>
           <a
             href="https://www.instagram.com/pacho_palumbo/"
             target="_blank"
             rel="noreferrer"
           >
-            <img height="40px" src={url + ('/images/instagram.png')} alt="insta" />
+            <img style={{height:'40px'}} src={url + ('/images/instagram.png')} alt="insta" />
           </a>
           <a
             href="https://github.com/Pacho499"
             target="_blank"
             rel="noreferrer"
           >
-            <img height="40px" src={url + ('/images/git.png')} alt="git" />
+            <img style={{height:'40px'}} src={url + ('/images/git.png')} alt="git" />
           </a>
         </div>
       </div>
 
       <div>
-        <p className="rights">
+        <p className="text-lg">
           LorenzoPalumboDeveloper &copy; all rights reserved{" "}
         </p>
       </div>
